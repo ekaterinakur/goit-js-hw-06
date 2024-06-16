73be3a97d600f356b5857e3615abfb84695ccea7
ekaterinakur <ekaterina1.kur@gmail.com>
2024-06-16 23:35:15 +0300

    [HW6] OOP

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,23 @@
 class Storage {
+  #items = [];
+
   constructor(initialItems) {
-    this._items = initialItems;
+    this.#items = initialItems;
   }
 
   getItems() {
-    return this._items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this._items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    const index = this._items.indexOf(itemToRemove);
-    
+    const index = this.#items.indexOf(itemToRemove);
+
     if (index !== -1) {
-      this._items.splice(index, 1);
+      this.#items.splice(index, 1);
     }
   }
 }
